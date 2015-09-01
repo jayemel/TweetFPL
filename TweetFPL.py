68484748533e3082d1b2dd1a62124dca631bbcac
jl1405 <jl1405@imperial.ac.uk>
2015-09-01 11:54:45 +0100

    Import Twitter authentication credentials from TweetFPLAuth.py.

--- TweetFPL.py
+++ TweetFPL.py
@@ -3,6 +3,7 @@
 ## A quick and dirty twitter bot that automatically tweets FPL2015 information
 
 import time, sched
+from TweetFPLAuth import *
 
 MESSAGE_FILE = 'Schedule.csv'
 START_TIME = time.time()
@@ -16,29 +17,19 @@ scheduler = sched.scheduler(time.time, time.sleep)
 
 # Open the file and load messages into a list
 messages = []
-# Previous trigger time and default priority
-priority = [0,1]
 with open(MESSAGE_FILE) as f:
 	for line in f:
-		# Throw away comments
-		if line[0] is not '#':
-			# Split the line into a list
-			data = line.rstrip().split(';')
-			# Convert the date and time into UTC
-			trigger = time.mktime(time.strptime(' '.join(data[0:2]), '%d/%m/%Y %H:%M'))
-			# Only accept future messages
-			if (trigger > START_TIME):
-				# Check if there is already a tweet at that time
-				if priority[0] is trigger:
-					# Increment the priority
-					priority[1] += 1
-				else:
-					# Reset the priority
-					priority[1] = 1
-				# Set the current trigger time
-				priority[0] = trigger
-				# Register the trigger
-				scheduler.enterabs(priority[0], priority[1], tweet_event, (data[-1],))
+		if line:
+			# Throw away comments
+			if line[0] is not '#':
+				# Split the line into a list
+				data = line.rstrip().split(';')
+				# Convert the date and time into UTC
+				trigger = time.mktime(time.strptime(' '.join(data[0:2]), '%d/%m/%Y %H:%M:%S'))
+				# Only accept future messages
+				if (trigger > START_TIME):
+					# Register the trigger
+					scheduler.enterabs(trigger, 1, tweet_event, (data[-1],))
 
 # Run the scheduler
 scheduler.run()
