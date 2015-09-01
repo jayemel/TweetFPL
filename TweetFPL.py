06b4878bf35db606879c8c2cf735595414441290
jl1405 <jl1405@imperial.ac.uk>
2015-09-01 10:57:30 +0100

    This should post messages at the scheduled times.

--- TweetFPL.py
+++ TweetFPL.py
@@ -7,19 +7,38 @@ import time, sched
 MESSAGE_FILE = 'Schedule.csv'
 START_TIME = time.time()
 
-scheduler = shed.scheduler(time.time, time.sleep)
+def tweet_event(message):
+	print "TWEET:", time.strftime('%d/%m/%Y %H:%M:%S', time.localtime()), message
+
+print 'Running TweetFPL, they FPL 2015 twitter message server.\n'
+
+scheduler = sched.scheduler(time.time, time.sleep)
 
 # Open the file and load messages into a list
 messages = []
+# Previous tri	gger time and default priority
+priority = [0,1]
 with open(MESSAGE_FILE) as f:
 	for line in f:
 		# Throw away comments
 		if line[0] is not '#':
 			# Split the line into a list
-			data = line.split(';')
+			data = line.rstrip().split(';')
 			# Convert the date and time into UTC
 			trigger = time.mktime(time.strptime(' '.join(data[0:2]), '%d/%m/%Y %H:%M'))
 			# Only accept future messages
 			if (trigger > START_TIME):
-				# Need to check if there is already a tweet at that time
-				
+				# Check if there is already a tweet at that time
+				if priority[0] is trigger:
+					# Increment the priority
+					priority[1] += 1
+				else:
+					# Reset the priority
+					priority[1] = 1
+				# Set the current trigger time
+				priority[0] = trigger
+				# Register the trigger
+				scheduler.enterabs(priority[0], priority[1], tweet_event, (data[-1],))
+
+# Run the scheduler
+scheduler.run()
